596eb223d10ffb7c5dd719b25a7d5f5566137c94
MJ Jacobs <marthinusjacobs@gmail.com>
2024-12-28 16:10:34 +0200

    Added extended icon dependency
    Took 38 seconds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     //  Compose
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.activity:activity-compose:1.9.3")
